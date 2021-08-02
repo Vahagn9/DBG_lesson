20389b2b16a74a1147c311b4ee6f205feeb83684
Vahagn <87897896+Vahagn9@users.noreply.github.com>
2021-08-02 10:38:41 +0400

    Add files via upload

--- hw_lesson1.py
+++ hw_lesson1.py
@@ -2,15 +2,11 @@
 from math import pi
 
 
+# ===== functions =====
 # ex. 1:
 def calculate_sphere_volume(radius):
     """Takes a radius, calculates and returns V(volume) of sphere"""
     return 4 / 2 * pi * radius ** 3
-
-
-print("exercise 1. Calculate V of sphere")
-r = float(input("please input a radius: "))
-print("The volume of a sphere of radius = ", r, "m is equal to: ", calculate_sphere_volume(r), "m**3", sep="")
 
 
 # ex. 2
@@ -34,22 +30,10 @@ def simple_interest_formula(principal: int, interest: int, time_period: int, t=1
     print("The interest for the entire term of the loan is :", interest)
 
 
-print("\nexercise 2. Simple interest formula")
-P = int(input("Input the initial amount of the deposit (loan): "))
-i = int(input("Input the simple interest rate (%): "))
-T = int(input("Input the loan term by month: "))
-simple_interest_formula(P, i, T)
-
-
 # ex. 3
 def calculate_sphere_area(radius):
     """Takes a radius, calculates and returns A(area) of sphere"""
     return 4 * pi * radius ** 2
-
-
-print("\nexercise 3. Area of Sphere")
-r = float(input("please input a radius: "))
-print("The area of a sphere of radius = ", r, "m is equal to: ", calculate_sphere_area(r), "m**2", sep="")
 
 
 # ex. 4
@@ -58,15 +42,34 @@ def two_numbers_pow(num_1: int, num_2: int, power=2):
     return (num_1 + num_2) ** power
 
 
-print("\nexercise 4. (a+b)**2")
-a = int(input("please input first number: "))
-b = int(input("please input second number: "))
-print("(", a, "+", b, ")**2 = ", two_numbers_pow(a, b))
+# ===== main =====
+ex_num = int(input("please input exercise number to run: "))
 
-# ex. 5
-print("\nexercise 5. Assign value of a variable to b variable without using third variable")
-a = int(input("please input first number: "))
-b = int(input("please input second number: "))
-print("before change: a =", a, "b =", b)
-a, b = b, a  # Actually uses 2 extra temp variables in back :)
-print("after change: a =", a, "b =", b)
+if ex_num <= 0 or ex_num > 5:
+    print("Wrong exercise number entered")
+elif ex_num == 1:  # ex. 1
+    print("exercise 1. Calculate V of sphere")
+    r = float(input("please input a radius: "))
+    print("The volume of a sphere of radius = ", r, "m is equal to: ", calculate_sphere_volume(r), "m**3", sep="")
+elif ex_num == 2:  # ex. 2
+    print("\nexercise 2. Simple interest formula")
+    P = int(input("Input the initial amount of the deposit (loan): "))
+    i = int(input("Input the simple interest rate (%): "))
+    T = int(input("Input the loan term by month: "))
+    simple_interest_formula(P, i, T)
+elif ex_num == 3:  # ex. 3
+    print("\nexercise 3. Area of Sphere")
+    r = float(input("please input a radius: "))
+    print("The area of a sphere of radius = ", r, "m is equal to: ", calculate_sphere_area(r), "m**2", sep="")
+elif ex_num == 4:  # ex. 4
+    print("\nexercise 4. (a+b)**2")
+    a = int(input("please input first number: "))
+    b = int(input("please input second number: "))
+    print("(", a, "+", b, ")**2 = ", two_numbers_pow(a, b))
+elif ex_num == 5:  # ex. 5
+    print("\nexercise 5. Assign value of a variable to b variable without using third variable")
+    a = int(input("please input first number: "))
+    b = int(input("please input second number: "))
+    print("before change: a =", a, "b =", b)
+    a, b = b, a  # Actually uses 2 extra temp variables in back :)
+    print("after change: a =", a, "b =", b)
